68eff6d468ecfa329091b833208458d336167748
Thiago Martins <thiago.fmartins@outlook.com>
2024-06-18 04:53:18 -0300

    Update NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
     <header className="bg-dark text-light navbar">
       <div className="container mx-auto flex justify-between items-center py-4">
         <h1 className="text-3xl font-bold neon-text">DeGym</h1>
-        <nav className="flex space-x-4">
+        <nav className="flex space-x-8">
           <Link href="/" legacyBehavior><a className="text-secondary hover:neon-green">About</a></Link>
-          <Link href="/token" legacyBehavior><a className="text-secondary hover:neon-green">DGYM Token</a></Link>
+          <Link href="/token" legacyBehavior><a className="text-secondary hover:neon-green">$DGYM</a></Link>
           <Link href="/presale" legacyBehavior><a className="text-secondary hover:neon-green">Presale</a></Link>
           <Link href="/membership-plans" legacyBehavior><a className="text-secondary hover:neon-green">Membership Plans</a></Link>
           <Link href="/become-gym-provider" legacyBehavior><a className="text-secondary hover:neon-green">Become Gym Provider</a></Link>

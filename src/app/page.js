77c7958ecd83d4f84fde93ede33bc4e215d31bd1
Thiago Martins <thiago.fmartins@outlook.com>
2024-06-21 09:44:54 -0300

    Update Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -28,12 +28,12 @@ const HomePage = () => {
         </section>
         <section className={styles.sectionEven}>
           <h2 className={styles.sectionEvenTitle}>Mission Statement</h2>
-          <p className={styles.paragraph}>To revolutionize fitness by providing seamless and decentralized access to gyms worldwide, empowering individuals to achieve their health and wellness goals anytime, anywhere.</p>
+          <p className={styles.paragraph}>At DeGym DAO, we envision a world where fitness enthusiasts have the freedom to access any gym, anytime, anywhere. By breaking down traditional membership barriers, we aim to create a global community that enjoys seamless gym access, fostering a healthier, more active lifestyle. Our vision is to eliminate physical and financial constraints, making health and wellness universally accessible and perfectly aligned with the digital age. By leveraging cutting-edge technology, we are committed to creating an interconnected network that supports and enhances the fitness journeys of individuals worldwide.</p>
         </section>
 
         <section className={styles.sectionOdd}>
           <h2 className={styles.sectionOddTitle}>Vision Statement</h2>
-          <p className={styles.paragraph}>To create a world where fitness is universally accessible, eliminating barriers and fostering a global community of health-conscious individuals.</p>
+          <p className={styles.paragraph}>Our mission at DeGym DAO is to revolutionize the fitness industry by leveraging Distributed Ledger Technology (DLT) to create a decentralized, transparent, and user-centric gym access ecosystem. We aim to empower gym users with seamless access to a worldwide network of gyms, ensuring flexibility and convenience. DeGym DAO provides gym partners with a platform that enhances engagement and optimizes facility utilization, while offering stakers fair and incentivized economic opportunities. By fostering a system of shared economic benefits, we strive to enhance the gym experience for everyone, making fitness more accessible, rewarding, and aligned with modern digital lifestyles. Our commitment is to build a global community where fitness is free from traditional constraints, promoting health, well-being, and an active lifestyle for all.</p>
         </section>
 
         <section className={styles.sectionEven}>

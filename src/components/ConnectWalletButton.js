953661a4e68416c4d44a2786c46ded7f4bf6af9d
Thiago Martins <thiago.fmartins@outlook.com>
2024-07-12 16:01:27 -0300

    Add network verification on wallet connection component

--- src/components/ConnectWalletButton.js
+++ src/components/ConnectWalletButton.js
@@ -1,13 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/components/ConnectWalletButton.module.css';
 
 const ConnectWalletButton = () => {
     const [walletAddress, setWalletAddress] = useState(null);
     const [error, setError] = useState('');
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isCorrectNetwork, setIsCorrectNetwork] = useState(true);
+
+    useEffect(() => {
+        const checkNetwork = async () => {
+            const provider = await detectEthereumProvider();
+            if (provider) {
+                const chainId = await provider.request({ method: 'eth_chainId' });
+                setIsCorrectNetwork(chainId === '0x349'); // Taraxa chainId
+            }
+        };
+
+        if (walletAddress) {
+            checkNetwork();
+        }
+
+        const handleChainChanged = (chainId) => {
+            setIsCorrectNetwork(chainId === '0x349');
+        };
+
+        if (window.ethereum) {
+            window.ethereum.on('chainChanged', handleChainChanged);
+        }
+
+        return () => {
+            if (window.ethereum) {
+                window.ethereum.removeListener('chainChanged', handleChainChanged);
+            }
+        };
+    }, [walletAddress]);
 
     const connectWallet = async () => {
         const provider = await detectEthereumProvider();
@@ -31,6 +60,7 @@ const ConnectWalletButton = () => {
                 if (accounts.length > 0) {
                     setWalletAddress(accounts[0]);
                     setError('');
+                    setIsCorrectNetwork(true);
                 }
             } catch (error) {
                 setError('Failed to connect wallet. Please try again.');
@@ -45,13 +75,17 @@ const ConnectWalletButton = () => {
         <div className={styles.walletContainer}>
             {walletAddress ? (
                 <div
-                    className={styles.walletInfo}
+                    className={`${styles.walletInfo} ${!isCorrectNetwork ? styles.incorrectNetwork : ''}`}
                     onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                     onMouseEnter={() => setIsDropdownOpen(true)}
                     onMouseLeave={() => setIsDropdownOpen(false)}
                 >
-                    <div className={styles.icon}>
-                        <img src="/img/hw/wallet.svg" alt="Wallet Icon" className="img-fluid d-block" />
+                    <div className={`${styles.icon} ${!isCorrectNetwork ? styles.iconIncorrectNetwork : ''}`}>
+                        {isCorrectNetwork ? (
+                            <img src="/img/hw/wallet.svg" alt="Wallet Icon" className="img-fluid d-block" />
+                        ) : (
+                            <FontAwesomeIcon icon={faExclamationTriangle} className={styles.warningIcon} />
+                        )}
                     </div>
                     <div className={styles.walletAddress}>
                         {walletAddress.substring(0, 6)}...{walletAddress.substring(walletAddress.length - 4)}

bd3923602110e324e64630bef60e49869a207506
Thiago Martins <thiago.fmartins@outlook.com>
2024-07-29 01:09:51 -0300

    Add mocked events (history of crowdfund)

--- src/app/pre-seed/CrowdfundingSection.js
+++ src/app/pre-seed/CrowdfundingSection.js
@@ -105,6 +105,7 @@ const CrowdfundingSection = ({ crowdfund, walletAddress }) => {
     const [web3, setWeb3] = useState(null);
     const [isCountdownActive, setIsCountdownActive] = useState(true);
     const [countdownTitle, setCountdownTitle] = useState(`${crowdfund.type} starts in:`);
+    const [filterMyEvents, setFilterMyEvents] = useState(false);
 
     useEffect(() => {
         if (walletAddress && window.ethereum) {
@@ -154,6 +155,33 @@ const CrowdfundingSection = ({ crowdfund, walletAddress }) => {
     };
 
     const isDisabled = isCountdownActive || !walletAddress;
+
+    const mockEvents = [
+        {
+            address: '0x86...a7AE',
+            amount: 4000000.0,
+            link: 'https://mainnet.explorer.taraxa.io/tx/0xf9f00be62995d5c67df6fcfc3b395aa9183ee54e807eeb5305c367b53f833b04'
+        },
+        {
+            address: '0xed...38E9',
+            amount: 400000.0,
+            link: 'https://mainnet.explorer.taraxa.io/tx/0x02a26fec12c3a341ad891410bb72c8e2b76fa287dd8f78beef3aba5463c72bdb'
+        },
+        {
+            address: '0xed...38E9',
+            amount: 800000.0,
+            link: 'https://mainnet.explorer.taraxa.io/tx/0x622bd987c704d2c0cf4f46f0d0cf470b612a70e52df38f4049b1d8527ee0065d'
+        },
+        {
+            address: '0xe3...6FBD',
+            amount: 3200000.0,
+            link: 'https://mainnet.explorer.taraxa.io/tx/0xcff7e06ead5fef58c94ba0f5940c5d549c430b7743e053542b2de869d4c40510'
+        }
+    ];
+
+    const filteredEvents = filterMyEvents && walletAddress
+        ? mockEvents.filter(event => event.address.includes(walletAddress.substring(2, 6)))
+        : mockEvents;
 
     return (
         <div className={styles.crowdfundingSection}>
@@ -234,6 +262,35 @@ const CrowdfundingSection = ({ crowdfund, walletAddress }) => {
                 <button onClick={handleGovernance} className="p-3">
                     Governance
                 </button>
+            </div>
+            <div className={styles.scrollableSection}>
+                <h2 className={styles.sectionTitle}>Latest swap events</h2>
+                <div className={styles.toggleContainer}>
+                    <label className={styles.toggleLabel}>
+                        <span>Just my events</span>
+                        <input
+                            type="checkbox"
+                            className={styles.toggle}
+                            checked={filterMyEvents}
+                            onChange={() => setFilterMyEvents(!filterMyEvents)}
+                        />
+                    </label>
+                </div>
+                <div className={styles.eventsContainer}>
+                    {filteredEvents.map((event, index) => (
+                        <div key={index} className={styles.eventCard}>
+                            <div className={styles.eventCardBody}>
+                                <div className={styles.eventActions}>
+                                    <a target="_blank" aria-label="Twitter" className={styles.eventLink} href={event.link}>View on Taraxa Explorer</a>
+                                </div>
+                                <div className={styles.eventDetails}>
+                                    <h2 className={styles.eventTitle}>{event.address}</h2>
+                                    <p className={styles.eventAmount}>{event.amount} DGYM</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );

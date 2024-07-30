9fa1ea47382c2f7f1aae023c2d8a3ad7e374ba07
Thiago Martins <thiago.fmartins@outlook.com>
2024-07-29 23:25:58 -0300

    Update bg particle network

--- src/components/ParticleNetworkAnimation.js
+++ src/components/ParticleNetworkAnimation.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef } from 'react';
-import styles from '../styles/ParticleNetwork.module.css';
+import styles from '@/styles/components/ParticleNetwork.module.css';
 
 class Particle {
     constructor(ctx, options) {

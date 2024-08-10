7973a970f2be33aa655df06c5546fea358dc9de5
Thiago Martins <thiago.fmartins@outlook.com>
2024-08-10 13:19:03 -0300

    Add private data Total Rewards to staking page

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = ({ title, value }) => {
     return (
         <div className="bg-accent p-5 rounded-lg shadow-md text-center flex-1 mx-2">
-            <h3 className="text-lg mb-2">{title}</h3>
-            <p className="text-xl font-bold">{value}</p>
+            <h2 className="text-3xl text-left font-bold mt-2 text-primary">{value}</h2>
+            <p className="text-sm text-left first-line:mb-2">{title}</p>
         </div>
     );
 };

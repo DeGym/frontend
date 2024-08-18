401d354bac8d235205f5421bcb249a03742e939c
Thiago Martins <thiago.fmartins@outlook.com>
2024-08-17 23:04:28 -0300

    commenting unused nav links

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -13,24 +13,30 @@ const Navigation: React.FC<NavigationProps> = ({ closeMenu }) => {
     return (
         <nav className={styles.navigation}>
             <ul className={styles.navList}>
+                {/*
                 <li className={styles.navItem}>
                     <Link href="/token" className={styles.link} onClick={handleClick}>Token</Link>
                 </li>
+                */}
                 <li className={styles.navItem}>
                     <Link href="/pre-seed" className={styles.link} onClick={handleClick}>Pre-Seed</Link>
                 </li>
+                {/*
                 <li className={styles.navItem}>
                     <Link href="/search" className={styles.link} onClick={handleClick}>Search</Link>
                 </li>
+                */}
                 <li className={styles.navItem}>
                     <Link href="/voucher" className={styles.link} onClick={handleClick}>Voucher</Link>
                 </li>
                 <li className={styles.navItem}>
                     <Link href="/partners" className={styles.link} onClick={handleClick}>Partners</Link>
                 </li>
+                {/*
                 <li className={styles.navItem}>
                     <Link href="/staking" className={styles.link} onClick={handleClick}>Staking</Link>
                 </li>
+                */}
                 <li className={styles.navItem}>
                     <Link href="https://docs.degym.net/" className={styles.link} onClick={handleClick}>Docs</Link>
                 </li>

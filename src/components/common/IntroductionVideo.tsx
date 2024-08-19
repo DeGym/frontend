3e1c9d9b1153c575ece1da8f3cd965e3611723ae
Thiago Martins <thiago.fmartins@outlook.com>
2024-08-19 19:01:19 -0300

    feat: Add Partners Carousel component and integrate it
    - Create new PartnersCarousel component
    - Add partner logos and styling
    - Integrate PartnersCarousel into index, members, and partners pages
    - Update styling for better responsiveness and visual appeal
    - Add new partner images to public directory

--- src/components/common/IntroductionVideo.tsx
+++ src/components/common/IntroductionVideo.tsx
@@ -10,15 +10,16 @@ const IntroductionVideo: React.FC = () => {
                 In a world where access to fitness facilities can be limited by location and time, DeGym emerges as a revolutionary platform breaking down these barriers.
                 By leveraging the power of decentralization, DeGym provides users with the freedom to access gyms and fitness centers worldwide, anytime they choose.
             </p>
-            <p>
-                Through innovation and community-building, We are redefining the fitness landscape, creating a global network of accessible, high-quality gym facilities.
-                It drives us to eliminate physical and financial constraints, making health and wellness seamlessly integrated into modern web 3.0 lifestyles.
-            </p>
+
             <h2>Watch Our <b>Introduction</b> Video</h2>
             <div className={styles.container}>
                 <div className={styles.videoOverlay}></div>
                 <video className={styles.video} controls controlsList="nodownload" src="videos/introduction.mp4"></video>
             </div>
+            <p>
+                Through innovation and community-building, We are <b>redefining the fitness landscape</b>, creating a global network of accessible, high-quality gym facilities.
+                It drives us to eliminate physical and financial constraints, making health and wellness seamlessly integrated into modern web 3.0 lifestyles.
+            </p>
         </section>
     );
 };
